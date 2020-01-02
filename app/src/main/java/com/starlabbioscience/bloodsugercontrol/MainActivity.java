ebf5f48589e9b8d6ff91093b13b54ac0fba67179
John Melody Me <johnmelodyme@icloud.com>
2020-01-02 14:26:10 +0800

    Upload

---
+++ app/src/main/java/com/starlabbioscience/bloodsugercontrol/MainActivity.java
@@ -0,0 +1,30 @@
+package com.starlabbioscience.bloodsugercontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @AUTHOR : JOHN MELODY ME || HAFIZAN
+ * @COPYRIGHT : BRAINTECH SDN BHD || STARLABS BIOSCIENCE SDN BHD
+ * @PROJECT: BLOOD SUGAR CONTROL
+ */
+
+public class MainActivity extends AppCompatActivity {
+    // UI- DECLARE:
+
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+
+    private void INIT_DECLARATION() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        INIT_DECLARATION();
+    }
+
+}
